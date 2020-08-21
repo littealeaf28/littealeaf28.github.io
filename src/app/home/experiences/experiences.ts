6f50940e97ddf38953bc5ed2c1a07ddf4cb3a964
Tianrui Li <lit1@ufl.edu>
2020-08-20 23:17:09 -0700

    Makes experience section mobile-friendly

--- src/app/home/experiences/experiences.ts
+++ src/app/home/experiences/experiences.ts
@@ -19,14 +19,14 @@ export const Experiences: Experience[] = [
       class: 'studio-reach'
     },
     company: 'Studio Reach',
-    position: 'Software Developer',
+    position: 'Software Developer Part-time',
     duration: 'June 2020 - Now',
-    description: '<p>- Implementing user stories for <a href="https://are-you-house-ready-dev.web.app/" target="_blank" class="ayhr-link">“Are You House Ready”</a>\n' +
-      'project in Angular 9 and Firebase and writing end-to-end and unit tests in Cypress and Jasmine for them</p>' +
-      '<p>- Helped develop a new layout for real estate vendor cards/pages, integrate\n' +
-      'a blog page as updated by an ETL job, and improve site’s SEO, among other features</p>' +
-      '<p>- Engages in daily standups, iteration planning meetings, retrospectives, and pair\n' +
-      'programming sessions as part of an Agile team with a kanban workflow based in Jira</p>',
+    description: '<li>Implementing user stories for <a href="https://are-you-house-ready-dev.web.app/" target="_blank" class="ayhr-link">“Are You House Ready”</a>\n' +
+      'project in Angular 9 and Firebase and writing end-to-end and unit tests in Cypress and Jasmine for them</li>' +
+      '<li>Helped develop a new layout for real estate vendor cards/pages, integrate\n' +
+      'a blog page as updated by an ETL job, and improve site’s SEO, among other features</li>' +
+      '<li>Engages in daily standups, iteration planning meetings, retrospectives, and pair\n' +
+      'programming sessions as part of an Agile team with a kanban workflow based in Jira</li>',
     skills: ['TypeScript', 'Angular 9', 'Firebase', 'Cypress', 'Jasmine', 'Jira']
   },
   {
@@ -38,11 +38,11 @@ export const Experiences: Experience[] = [
     company: 'UF CISE',
     position: 'Applications of Discrete Structures Teaching Assistant',
     duration: 'January 2020 - Now',
-    description: '<p>- Prepares powerpoint presentations and example problems for brief lecture reviews over core concepts during weekly discussions</p>' +
-      '<p>- Addresses student questions and concerns during weekly office hours</p>' +
-      '<p>- Engages in weekly conferences with the professor and other fellow TAs to plan out the logistics of the course,\n' +
-      'such as times to grade and course content being reviewed</p>' +
-      '<p>- Develops rubrics for grading questions with other TAs</p>',
+    description: '<li>Prepares powerpoint presentations and example problems for brief lecture reviews over core concepts during weekly discussions</li>' +
+      '<li>Addresses student questions and concerns during weekly office hours</li>' +
+      '<li>Engages in weekly conferences with the professor and other fellow TAs to plan out the logistics of the course,\n' +
+      'such as times to grade and course content being reviewed</li>' +
+      '<li>Develops rubrics for grading questions with other TAs</li>',
     skills: []
   },
   {
@@ -54,15 +54,15 @@ export const Experiences: Experience[] = [
     company: 'Bookmark\'d',
     position: 'Full-stack Web Developer',
     duration: 'October 2019 - July 2020',
-    description: '<p>- Visit the website now in testing: <a href="https://bookmarkdofficial.com/" target="_blank" class="bookmarkd-link">"BookMark\'d"</a></p>' +
-      '<p>- Developed microservices as part of a REST API using Node + Express\n' +
-      '(e.g. user authentication and authorization, product searching, and payment processing) and documents in Confluence</p>' +
-      '<p>- Leverages a variety of 3rd party APIs and software (Passport.js, Elasticsearch, Square, Amazon SES, etc.)\n' +
-      'and manages database of user and products through MongoDB</p>' +
-      '<p>- Managed web app infrastructure through AWS services: runs app on EC2 instances, develops a pipeline for CI/CD\n' +
-      'via CodeCommit, CodeDeploy + CodePipeline, and sets up domain name and SSL certificate with Route 53 and Amazon Certificate Manager</p>' +
-      '<p>- Implemented a minimalistic user interface through React for displaying user information and providing\n' +
-      'access to appropriate functionality/services</p>',
+    description: '<li>Visit the website now in testing: <a href="https://bookmarkdofficial.com/" target="_blank" class="bookmarkd-link">"BookMark\'d"</a></li>' +
+      '<li>Developed microservices as part of a REST API using Node + Express\n' +
+      '(e.g. user authentication and authorization, product searching, and payment processing) and documents in Confluence</li>' +
+      '<li>Leverages a variety of 3rd party APIs and software (Passport.js, Elasticsearch, Square, Amazon SES, etc.)\n' +
+      'and manages database of user and products through MongoDB</li>' +
+      '<li>Managed web app infrastructure through AWS services: runs app on EC2 instances, develops a pipeline for CI/CD\n' +
+      'via CodeCommit, CodeDeploy + CodePipeline, and sets up domain name and SSL certificate with Route 53 and Amazon Certificate Manager</li>' +
+      '<li>Implemented a minimalistic user interface through React for displaying user information and providing\n' +
+      'access to appropriate functionality/services</li>',
     skills: ['React', 'Node', 'Express', 'AWS']
   }
 ];
