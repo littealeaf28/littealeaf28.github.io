66c4cff4cf0344d4ff93fc20511ef4ae0c058e64
Tianrui Li <tealeaf@icari.io>
2022-08-02 07:18:05 -0700

    Fixes some errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,6 @@ import {ExperiencesComponent} from './home/experiences/experiences.component';
 import {AngularSvgIconModule} from 'angular-svg-icon';
 import {ExperienceCardComponent} from './home/experiences/experience-card/experience-card.component';
 import {SkillsHotbarComponent} from './home/shared/skills-hotbar/skills-hotbar.component';
-import {ProjectsComponent} from './home/projects/projects.component';
-import {ProjectDisplayComponent} from './home/projects/project-display/project-display.component';
-import {ProjectCardComponent} from './home/projects/project-card/project-card.component';
 import {ExcitingComponent} from './exciting/exciting.component';
 import {FunCarouselComponent} from './exciting/fun-carousel/fun-carousel.component';
 import {TopicBulletinComponent} from './exciting/topic-bulletin/topic-bulletin.component';
@@ -31,9 +28,6 @@ import { ExperienceImageComponent } from './home/experiences/experience-card/exp
     ExperiencesComponent,
     ExperienceCardComponent,
     SkillsHotbarComponent,
-    ProjectsComponent,
-    ProjectDisplayComponent,
-    ProjectCardComponent,
     ExcitingComponent,
     FunCarouselComponent,
     TopicBulletinComponent,
